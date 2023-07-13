b02e602bfb56be587c99c09a110b0e13dc344d42
ValentineSokol <vallyull98@gmail.com>
2023-07-13 14:19:57 +0300

    nav logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,15 +28,7 @@ function classNames(...classes) {
 
 export default function Example() {
   return (
-      <>
-        {/*
-        This example requires updating your template:
-
-        ```
-        <html class="h-full bg-gray-100">
-        <body class="h-full">
-        ```
-      */}
+      <main>
         <div className="min-h-full">
           <Disclosure as="nav" className="bg-gray-800">
             {({ open }) => (
@@ -44,12 +36,8 @@ export default function Example() {
                   <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                     <div className="flex h-16 items-center justify-between">
                       <div className="flex items-center">
-                        <div className="flex-shrink-0">
-                          <img
-                              className="h-8 w-8"
-                              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                              alt="Your Company"
-                          />
+                        <div className="flex-shrink-0 w-16 h-16">
+                         <Image src={Logo} alt={'логотип'} />
                         </div>
                         <div className="hidden md:block">
                           <div className="ml-10 flex items-baseline space-x-4">
@@ -180,6 +168,6 @@ export default function Example() {
             <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">{/* Your content */}</div>
           </main>
         </div>
-      </>
+      </main>
   )
 }

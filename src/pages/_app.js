1df62168a69058605274fafd536ff3569aad0c42
ValentineSokol <vallyull98@gmail.com>
2023-07-13 14:24:18 +0300

    Extract the Navbar component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,10 @@
 import '@/styles/globals.css'
+import Navbar from "@/components/Navbar/Navbar";
 
 export default function App({ Component, pageProps }) {
   return (
       <>
+      <Navbar />
       <Component {...pageProps} />
       </>
   )
